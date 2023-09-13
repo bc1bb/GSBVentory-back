f9bde3e6d5c12cf54530e6afa22549014688fb29
jusdepatate <jusdepatate@protonmail.com>
2023-09-13 12:05:20 +0200

    authent working

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 ## db
 - `use gsbv`
 - `db.createUser({ user: "gsbv", pwd: "gsbv", roles:["dbAdmin"]})`
+- `db.users.insertOne({username: "john", password: "$2y$10$ZiVn1Phi9H5yYAPO34VL9e/Hi1edPXU5QGtP/nmRAkG8ETv/Pkqiy", userType: 4})`
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-
+import cors from 'cors';
 
 const jwt_token = process.env.JWT_TOKEN;
 const db_url = process.env.DB_URL;
@@ -11,21 +11,26 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 // Middleware pour le parsing du JSON
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(cors());
 
 // Connexion à la base de données MongoDB
 mongoose.connect(db_url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+}).then(r => console.log("Connecté à la base de donnée"));
 
 // Modèle d'utilisateur MongoDB
-const User = mongoose.model('User', {
-    username: String,
-    password: String,
-    userType: Number,
+const userSchema = new mongoose.Schema({
+    username: {type: String, index: true, unique: true},
+    password: {type: String},
+    userType: {type: Number},
     // 1 pour employé de laboratoire, 2 pour technicien, 3 pour directeur, 4 pour super utilisateur
 });
+userSchema.index({ username: 1 }, { sparse: true });
+const User = mongoose.model('user', userSchema);
 
 // Route de healthcheck
 app.get('/healthcheck', (req, res) => {
@@ -36,22 +41,31 @@ app.get('/healthcheck', (req, res) => {
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
-    const user = await User.findOne({ username });
+    const user = await User.findOne({username});
 
     if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé' });
+        return res.status(401).json({ error: 401 });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-        return res.status(401).json({ error: 'Mot de passe incorrect' });
+        return res.status(401).json({ error: 401 });
     }
 
     const token = jwt.sign({ username, userType: user.userType }, jwt_token);
 
     res.json({ token });
 });
+
+// Fonction pour hasher les mots de passes facilement
+const hashPassword = async (password) => {
+    const hashRounds = 10;
+
+    const salt = await bcrypt.genSalt(hashRounds);
+
+    return await bcrypt.hash(password, salt);
+}
 
 // Middleware pour l'authentification
 const authenticate = (req, res, next) => {
@@ -74,9 +88,9 @@ const authenticate = (req, res, next) => {
 // Endpoint privé pour les utilisateurs connectés
 app.get('/private', authenticate, (req, res) => {
     res.json({ message: 'Bienvenue sur l\'endpoint privé' });
-    });
+});
 
 // Démarrage du serveur
-    app.listen(port, () => {
-        console.log(`Serveur en cours d'exécution sur le port ${port}`);
-    });
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+});
